4786f25f50c5703f00846c6e8e83303bf527b498
Tan Kim Yong <kimyong95@gmail.com>
2026-08-16 14:52:51 +0800

    update

--- config/optimize-hidden.py
+++ config/optimize-hidden.py
@@ -10,21 +10,21 @@ def get_config():
 
     config.model = ml_collections.ConfigDict()
     config.model.name = "Qwen/Qwen3-1.7B"
-    config.model.max_batch_size_per_device = 128
+    config.model.max_batch_size_per_device = 64
     config.model.max_new_tokens = 4096
     config.model.enable_thinking = False
     config.model.temperature = 1.0
     config.model.n_intervene = 8
 
     config.sample = ml_collections.ConfigDict()
-    config.sample.task = "dapo-math-17k"
+    config.sample.task = "math-500"
     config.sample.total_samples = 512
     config.sample.m = 32 # number of distinct prompts
 
     config.val = ml_collections.ConfigDict()
     config.val.task = "aime-2024"
     config.val.every_n_epochs = 25
-    config.val.k = 32 # Pass@K
+    config.val.k = 4 # Pass@K
 
     config.train = ml_collections.ConfigDict()
     config.train.learning_rate = 0.03
